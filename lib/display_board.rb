1b17cffa7a1cc2f141bc48d80e3c2a0052ceea63
Catherine <catkcm.16@gmail.com>
2019-04-14 13:07:04 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,36 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+# Building the board
+
+# def display_board
+#   puts "   |   |   "
+#   puts "-----------"
+#   puts "   |   |   "
+#   puts "-----------"
+#   puts "   |   |   "
+# end
+
+# print display_board
+
+n = ["X", " ", "O", " ", "X", " ", " ", " ", "X"]
+# puts board
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+display_board(n)
+
+
+## Code to populate board array with 9.times of " " 
+# 9.times do
+# board.push(" ")
+# end
+
+# print board
+
---
+++ test.rb
@@ -0,0 +1,27 @@
+# print all numbers from 1-100
+
+for n in 1..100
+  puts n
+end
+
+
+#solution:
+100.times do |num|
+  puts num+1
+end
+
+# print even numbers
+
+for evennum in 0..98
+  if evennum
+  puts even+2
+end
+
+#solution
+100.times do |num|
+  if (num+1).even?
+    puts (num+1)
+  end
+end
+
+50.times do |num|
